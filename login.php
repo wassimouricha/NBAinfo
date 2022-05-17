1e1d19cc1ce0164008fc86579553c82a045eb73b
wassimouricha <wbouricha5@gmail.com>
2022-05-17 15:32:11 +0200

    login page modifying

--- login.php
+++ login.php
@@ -15,14 +15,14 @@
             <!-- sidebar -->
             <?php include 'includes/sidebar.php' ; ?>
             <!-- formulaire -->
-            <div class="containere">
+            <div class="containereform">
         
             <?php 
 
     if(isset($errorMsg)){echo '<p class="messagus">'.$errorMsg.'</p>'; }  ?>
         <br> <br>
-    <form class="container" method="POST"> <!-- ici j'ajoute la method HTTP post afin d'indiquer que je vais envoyer des informations -->
-        <div class="mb-3">
+    <div class="containerloginform" > <!-- ici j'ajoute la method HTTP post afin d'indiquer que je vais envoyer des informations -->
+        <!-- <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Pseudo</label>
             <input type="text" class="form-control" name="pseudo">
             <div id="emailHelp" class="form-text">Mettez votre pseudo</div>
@@ -36,11 +36,33 @@
 
         <button type="submit" class="btn btn-dark" name="validate">Se connecter</button>
         <br> <br>
-        <a href="page_de_connexion.php"><p >pas de compte ? Inscrivez-vous !</p></a>
-    </form>
+        <a href="page_de_connexion.php"><p >pas de compte ? Inscrivez-vous !</p></a> -->
+
+        <!-- Formulaire version plus belle -->
+        <div class="form-container">
+            <div class="signin-signup">
+                <form  class="sign-in-form" method="POST">
+                    <h2 class="titreformulaire"> Connexion</h2>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input type="text" placeholder="Pseudo" name="pseudo">
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-lock"></i>
+                        <input type="password" placeholder="Mot de passe" name="password">
+                    </div>
+                    <button type="submit" class="btn btn-dark" name="validate">Se connecter</button>
+                </form>
+            </div>
+        </div>
+        <div class="panels-container">
+
+
+</div>
 
 
 
+        </div>
 
 
 
